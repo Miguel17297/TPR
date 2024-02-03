d8ec1a71bebd6a4696216e79665ae1f0266a49f0
Guilherme Claro <gui@ua.pt>
2024-02-03 23:13:11 +0000

    esemble algorithm missing

--- data/anomaly_detectionv2.py
+++ data/anomaly_detectionv2.py
@@ -132,7 +132,8 @@ def main(bot, pca):
         os.makedirs(results_path)
 
     if pca:
-        pca_values = [*range(6,27)]
+        # pca_values = [*range(17,21)]
+        pca_values= [17]
 
         for i in pca_values:
             file_name = os.path.join(results_path, f'pca_{i}.txt')
--- data/essemble.py
+++ data/essemble.py
@@ -10,12 +10,12 @@ from utils import distance, dataset_division,validate_model
 import os
 import sys
 
-BEST_NU = 0.1
+BEST_NU = 0.4
 BEST_THRESHOLD = 10
 BEST_MS = 400
-BEST_RS = 7
+BEST_RS = 3
 
-def ensemble(bot, pca, outfile):
+def ensemble(bot):
     original_stdout = sys.stdout
     
     results_path = os.path.join(os.path.dirname(os.getcwd()), "results")
@@ -24,37 +24,65 @@ def ensemble(bot, pca, outfile):
     if not os.path.exists(results_path):
         os.makedirs(results_path)
 
+    features_linked = np.loadtxt("linkedIn.dat")
+    features_trip = np.loadtxt("tripadvisor.dat")
+    features_tb = np.loadtxt("taobao.dat")
+    
+    train_linked, test_linked = dataset_division(features_linked)
+    train_trip, test_trip = dataset_division(features_trip)
+    train_taobao, test_taobao = dataset_division(features_tb)
+    
     features_bot = np.loadtxt(f'bot{bot}.dat')
-    features_normal = np.vstack((np.loadtxt("linkedIn.dat"),np.loadtxt("tripadvisor.dat"),np.loadtxt("taobao.dat")))
-    train_normal, test_normal = dataset_division(features_normal)
     train_bot, test_bot = dataset_division(features_bot)
+
+
+
+    train_bot, test_bot = dataset_division(features_bot)
+    train_normal = np.vstack((train_linked, train_trip, train_taobao))
+
+    test_normal = np.vstack((test_linked, test_trip, test_taobao))
+    
+
     labels_normal_test = np.ones((len(test_normal), 1)) * 1
     labels_bot_test = np.ones((len(test_bot), 1)) * -1
 
     test_data = np.vstack((test_normal, test_bot))
     test_labels = np.vstack((labels_normal_test, labels_bot_test))
 
+    test_data = np.vstack((test_normal, test_bot))
+    test_labels = np.vstack((labels_normal_test, labels_bot_test))
+    
+    pca_27= PCA(n_components=27)
+    pca_23 = PCA(n_components=23)
+    train_normal_pca_27= pca_27.fit_transform(train_normal)
+    test_normal_pca_27 = pca_27.fit_transform(test_data)
+    train_normal_pca_23= pca_23.fit_transform(train_normal)
+    test_normal_pca_23 = pca_23.fit_transform(test_data)
+
+    
+
     sm = StatisticalModel(BEST_THRESHOLD)
-    sm.train(train_normal)
-    sm_res = sm.predict(test_data)
+    sm.train(train_normal_pca_27)
+    sm_res = sm.predict(test_normal_pca_27)
 
     ocsvm = OneClassSVM("linear", BEST_NU)
-    ocsvm.train(train_normal)
-    ocsvm_res = ocsvm.predict(test_data)
+    ocsvm.train(train_normal_pca_23)
+    ocsvm_res = ocsvm.predict(test_normal_pca_23)
 
     rbf_ocsvm = OneClassSVM("rbf", BEST_NU)
-    rbf_ocsvm.train(train_normal)
-    rbf_res = rbf_ocsvm.predict(test_data)
+    rbf_ocsvm.train(train_normal_pca_23)
+    rbf_res = rbf_ocsvm.predict(test_normal_pca_23)
 
     poly_ocsvm = OneClassSVM("poly", BEST_NU)
-    poly_ocsvm.train(train_normal)
-    poly_res = poly_ocsvm.predict(test_data)
+    poly_ocsvm.train(train_normal_pca_23)
+    poly_res = poly_ocsvm.predict(test_normal_pca_23)
 
     isf = IsolationForest(BEST_MS, BEST_RS)
     isf.train(train_normal)
     isf_res = isf.predict(test_data)
 
     essemble_res = np.ones(len(test_data))
+    
 
     with open(os.path.join(results_path,"ensemble.txt"), "w") as f:
         sys.stdout = f
@@ -81,13 +109,9 @@ def ensemble(bot, pca, outfile):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Anomaly detection')
     parser.add_argument('-b', '--bot', type=int, help='Bot Level', choices=[1, 2, 3], default=3)
-    parser.add_argument('-o', '--output', type=str,nargs='?', help='Output file',const='ensemble.txt')
-    parser.add_argument('--pca', type=bool, help='Use pca', default=False)
     args = parser.parse_args()
     
-
     bot = args.bot
-    outfile = args.output
-    pca = args.pca
+
     
-    ensemble(bot, pca, outfile)
+    ensemble(bot)
